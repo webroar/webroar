1cd5adefc63fc3b3bac071391010374255e30dc2
Dharmarth Shah <dharmarth.shah@webroar.in>
2009-11-26 17:23:31 +0530

    Resolving 'starling-starling' dependency using gem method.

--- src/ruby_lib/analyzer/message_reader.rb
+++ src/ruby_lib/analyzer/message_reader.rb
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with WebROaR.  If not, see <http://www.gnu.org/licenses/>.
 
+gem 'starling-starling', '>=0.10.0'
 require 'starling'
 
 module Webroar
