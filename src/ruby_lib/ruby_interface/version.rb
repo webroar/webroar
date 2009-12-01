c9f56272b1c9bcfe9343e80993e15db3b4de1ace
Dharmarth Shah <dharmarth.shah@webroar.in>
2009-12-01 22:15:47 +0530

    Updated version to 2.3.4

--- src/ruby_lib/ruby_interface/version.rb
+++ src/ruby_lib/ruby_interface/version.rb
@@ -21,7 +21,7 @@ module Webroar
   module VERSION #:nodoc:
     MAJOR    = "0"
     MINOR    = "2"
-    TINY     = "3"
+    TINY     = "4"
     
     STRING   = [MAJOR, MINOR, TINY].join('.')
   end
