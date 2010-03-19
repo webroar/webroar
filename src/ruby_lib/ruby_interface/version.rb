0ebcb7ff5ba4f0ec801d6462933bcc34e44be83f
Nikunj Limbaseeya <nikunj.limbaseeya@webroar.in>
2010-03-20 00:32:31 +0530

    Updated version to v0.3.1

--- src/ruby_lib/ruby_interface/version.rb
+++ src/ruby_lib/ruby_interface/version.rb
@@ -21,7 +21,7 @@ module Webroar
   module VERSION #:nodoc:
     MAJOR    = "0"
     MINOR    = "3"
-    TINY     = "0"
+    TINY      = "1"
     
     STRING   = [MAJOR, MINOR, TINY].join('.')
   end
