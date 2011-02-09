71fbaa6beabc52da4834af094d3accb691cd507e
Nikunj Limbaseeya <nikunj.limbaseeya@webroar.in>
2011-02-09 18:36:09 +0530

    Updated YAML parser unit test cases.

--- test/unit/yaml_test.c
+++ test/unit/yaml_test.c
@@ -26,113 +26,88 @@
 #include <test.h>
 
 /** Test with key value pairs */
-void test_key_value_pairs()
-{
+void test_key_value_pairs() {
   node_t* root = yaml_parse("config/config1.yml");
 
   UT_ASSERT_PTR_NOT_NULL(root)
-  UT_ASSERT_STRING_EQUAL(get_node_value(root,"item"),"keybord")
-  UT_ASSERT_STRING_EQUAL(get_node_value(root,"price"),"100")
-  UT_ASSERT_PTR_NULL(get_node_value(root,"code"))
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(root,"item"),"keybord")
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(root,"price"),"100")
+  UT_ASSERT_PTR_NULL(yaml_get_value(root,"code"))
 
-  if(root)
-  {
-    node_free(root);
-  }
+  yaml_node_free(root);
 }
 
 /** Test with sequence */
-void test_sequence()
-{
+void test_sequence() {
   node_t *root = yaml_parse("config/config2.yml");
-  node_t *node;
+  node_t *node, *tmp;
 
   UT_ASSERT_PTR_NOT_NULL(root)
-  node = get_nodes(root,"Item List/item");
-  UT_ASSERT_STRING_EQUAL(NODE_VALUE(node),"keybord")
-  node = NODE_NEXT(node);
-  UT_ASSERT_STRING_EQUAL(NODE_VALUE(node),"mouse")
+  node = yaml_get_node(root, "Item List/*");
 
-  node = get_nodes(root,"Item List/price");
-  UT_ASSERT_STRING_EQUAL(NODE_VALUE(node),"100")
-  node = NODE_NEXT(node);
-  UT_ASSERT_STRING_EQUAL(NODE_VALUE(node),"50")
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(node->child,"item"), "keybord")
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(node->child,"price"), "100")
 
-  node = NODE_NEXT(node);
+  node = node->next;
+
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(node->child,"item"), "mouse")
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(node->child,"price"), "50")
+
+  node = node->next;
   UT_ASSERT_PTR_NULL(node)
 
-  if(root)
-  {
-    node_free(root);
-  }
+  yaml_node_free(root);
 }
 
 /** Test with nested sequence */
-void test_nested_sequence()
-{
+void test_nested_sequence() {
   node_t *root = yaml_parse("config/config3.yml");
-  node_t *node;
+  node_t *node, *tmp;
 
   UT_ASSERT_PTR_NOT_NULL(root)
-  node = get_nodes(root,"Item List/item");
-  UT_ASSERT_STRING_EQUAL(NODE_VALUE(node),"keybord")
-  node = NODE_NEXT(node);
-  UT_ASSERT_STRING_EQUAL(NODE_VALUE(node),"mouse")
-  node = NODE_NEXT(node);
+  node = yaml_get_node(root, "Item List/*");
 
-  node = get_nodes(root,"Item List/price");
-  UT_ASSERT_STRING_EQUAL(NODE_VALUE(node),"100")
-  node = NODE_NEXT(node);
-  UT_ASSERT_STRING_EQUAL(NODE_VALUE(node),"50")
-  node = NODE_NEXT(node);
+  tmp = node->child;
 
-  node = get_nodes(root,"Item List/model/company");
-  UT_ASSERT_STRING_EQUAL(NODE_VALUE(node),"iball")
-  node = NODE_NEXT(node);
-  UT_ASSERT_STRING_EQUAL(NODE_VALUE(node),"sony")
-  node = NODE_NEXT(node);
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(tmp, "item"),"keybord")
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(tmp, "price"),"100")
 
-  node = get_nodes(root,"Item List/model/keys");
-  UT_ASSERT_STRING_EQUAL(NODE_VALUE(node),"58")
-  node = NODE_NEXT(node);
-  UT_ASSERT_STRING_EQUAL(NODE_VALUE(node),"54")
-  node = NODE_NEXT(node);
+  tmp = yaml_get_node(root, "Item List/*/model/*");
 
-  UT_ASSERT_STRING_EQUAL(get_node_value(root,"Billing Address/Item"),"cpu")
-  UT_ASSERT_STRING_EQUAL(get_node_value(root,"Billing Address/price"),"1500")
+  UT_ASSERT_STRING_EQUAL(tmp->key,"iball")
+  UT_ASSERT_STRING_EQUAL(tmp->next->key,"sony")
 
+  node = node->next;
+  tmp = node->child;
 
-  if(root)
-  {
-    node_free(root);
-  }
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(tmp, "item"),"mouse")
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(tmp, "price"),"50")
+
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(root,"Billing Address/*/Item"),"cpu")
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(root,"Billing Address/*/price"),"1500")
+
+  yaml_node_free(root);
 }
 
 /** Test with test file */
-void test_text_file()
-{
+void test_text_file() {
   node_t* root = yaml_parse("config/config4.yml");
 
   UT_ASSERT_PTR_NULL(root)
 }
 
-void test_space_separated_value()
-{
+void test_space_separated_value() {
   node_t* root = yaml_parse("config/config5.yml");
 
   UT_ASSERT_PTR_NOT_NULL(root)
-  UT_ASSERT_STRING_EQUAL(get_node_value(root,"item"),"mouse keybord")
-  UT_ASSERT_STRING_EQUAL(get_node_value(root,"price"),"100")
-  UT_ASSERT_PTR_NULL(get_node_value(root,"code"))
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(root,"item"),"mouse keybord")
+  UT_ASSERT_STRING_EQUAL(yaml_get_value(root,"price"),"100")
+  UT_ASSERT_PTR_NULL(yaml_get_value(root,"code"))
 
-  if(root)
-  {
-    node_free(root);
-  }
+  yaml_node_free(root);
 }
 
-void test_yaml_parser()
-{
+void test_yaml_parser() {
   UT_TEST_START
   UT_RUN_TEST(test_key_value_pairs)
   UT_RUN_TEST(test_sequence)
