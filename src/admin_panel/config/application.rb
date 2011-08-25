38fef34342b6c97e17bd22c20686335e7e148d2c
Hokam Singh Chauhan <hokam.chauhan@webroar.in>
2011-08-25 18:19:03 +0530

    Requiring 'yaml_writer' and 'control' files in application.rb file.

--- src/admin_panel/config/application.rb
+++ src/admin_panel/config/application.rb
@@ -26,7 +26,7 @@ module AdminPanel
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'UTC'
-   
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
@@ -42,3 +42,5 @@ module AdminPanel
   end
 end
 require "asset_tag_helper_ext"
+require 'yaml_writer'
+require 'control'
