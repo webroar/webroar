928a14dc65d51662133615841d152415b7b484d5
Nikunj Limbaseeya <nikunj.limbaseeya@webroar.in>
2011-09-06 16:48:26 +0530

    Load Admin Panel from Ruby script.

--- lib/constant.rb
+++ lib/constant.rb
@@ -16,19 +16,26 @@
 # You should have received a copy of the GNU General Public License
 # along with WebROaR.  If not, see <http://www.gnu.org/licenses/>.
 
-WEBROAR_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-WEBROAR_LIB_DIR = File.expand_path(File.join(WEBROAR_ROOT, 'lib'))
-WEBROAR_TEST_DIR = File.expand_path(File.join(WEBROAR_ROOT, 'test', 'unit'))
-WEBROAR_BIN_DIR = File.expand_path(File.join(WEBROAR_ROOT, 'bin'))
-ADMIN_PANEL_DIR = File.join(WEBROAR_ROOT, 'src', 'admin_panel')
-ADMIN_PANEL_LIB_DIR = File.join(ADMIN_PANEL_DIR, 'lib')
-RAILS_ROOT = ADMIN_PANEL_DIR
-GEM_BIN_DIR = File.expand_path(File.join(WEBROAR_ROOT,'..','..','bin'))
-USR_BIN_DIR = File.join('','usr','bin')
-ANALYZER_DIR = File.join(WEBROAR_ROOT, 'src', 'ruby_lib', 'analyzer')
-INTERNAL_CONF_FILE = File.join(WEBROAR_ROOT, 'conf', 'server_internal_config.yml')
-ENV["PATH"] += ":/usr/bin:/usr/sbin:/sbin:/usr/local/sbin:#{WEBROAR_BIN_DIR}"
-WEBROAR_LOG_FILE = File.join('','var','log','webroar','webroar.log')
-PIDFILE = "/var/run/webroar.pid"
-TESTFILE = "/etc/profile"
-MESSAGE_DEPLOYMENT = "This command needs to be run as root. Please try again using 'sudo'.".freeze
+# Directory constant
+WEBROAR_ROOT          = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+WEBROAR_LIB_DIR       = File.expand_path(File.join(WEBROAR_ROOT, 'lib'))
+WEBROAR_TEST_DIR      = File.expand_path(File.join(WEBROAR_ROOT, 'test', 'unit'))
+WEBROAR_BIN_DIR       = File.expand_path(File.join(WEBROAR_ROOT, 'bin'))
+ADMIN_PANEL_DIR       = File.join(WEBROAR_ROOT, 'src', 'admin_panel')
+ADMIN_PANEL_LIB_DIR   = File.join(ADMIN_PANEL_DIR, 'lib')
+#RAILS_ROOT = ADMIN_PANEL_DIR
+GEM_BIN_DIR           = File.expand_path(File.join(WEBROAR_ROOT,'..','..','bin'))
+USR_BIN_DIR           = File.join('','usr','bin')
+ANALYZER_DIR          = File.join(WEBROAR_ROOT, 'src', 'ruby_lib', 'analyzer')
+
+
+ENV["PATH"]           += ":/usr/bin:/usr/sbin:/sbin:/usr/local/sbin:#{WEBROAR_BIN_DIR}"
+
+# File constant
+INTERNAL_CONF_FILE    = File.join(WEBROAR_ROOT, 'conf', 'server_internal_config.yml')
+WEBROAR_LOG_FILE      = File.join('','var','log','webroar','webroar.log')
+PIDFILE               = "/var/run/webroar.pid"
+TESTFILE              = "/etc/profile"
+
+# Message constant
+MESSAGE_DEPLOYMENT    = "This command needs to be run as root. Please try again using 'sudo'.".freeze
