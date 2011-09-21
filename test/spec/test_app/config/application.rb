0ff063f955f593a182e78faffa22ebc280395559
Hokam Singh Chauhan <hokam.chauhan@webroar.in>
2011-09-21 17:32:37 +0530

    Loading only required resources in Test Application.

--- test/spec/test_app/config/application.rb
+++ test/spec/test_app/config/application.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require 'rails'
+require 'active_record/railtie'
+require "action_controller/railtie"
+
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
